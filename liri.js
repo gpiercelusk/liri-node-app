ef913fa0a9b6dec38ed1e59b4fc769725e318b87
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-04-14 19:22:05 -0400

    set a switch function and variables

--- liri.js
+++ liri.js
@@ -1,5 +1,46 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
+const fs = require("fs");
 
-var axios = require("axios");
+const keys = require("./keys.js");
+
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+
+const axios = require("axios");
+
+const moment = require("moment");
+
+let input = process.argv;
+let command = process.argv[2];
+
+let name = "";
+
+switch (command) {
+  case "concert-this":
+    setName();
+    concertThis(name);
+    break;
+
+  case "spotify-this-song":
+    setName();
+    spotifyThis(name);
+    break;
+
+  case "movie-this":
+    setName();
+    movieThis(name);
+    break;
+
+  case "do-what-it-says":
+    doThis();
+    break;
+
+  default:
+    console.log("");
+    console.log("Your choices are: 'concert-this, 'spotify-this-song', 'movie-this', 'do-what-it-says'");
+    console.log("");
+
+
+
+}
