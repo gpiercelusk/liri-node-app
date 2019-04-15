9b1db659657dae9713a649ea76ba977c3a851558
G. Pierce Lusk <gpiercelusk@gmail.com>
2019-04-14 21:57:56 -0400

    concert this and spotifythis function working

--- liri.js
+++ liri.js
@@ -38,9 +38,79 @@ switch (command) {
 
   default:
     console.log("");
-    console.log("Your choices are: 'concert-this, 'spotify-this-song', 'movie-this', 'do-what-it-says'");
+    console.log("Your choices are: 'concert-this (artist)', 'spotify-this-song (song)', 'movie-this (movie)', 'do-what-it-says'");
     console.log("");
+}
 
+function setName() {
+  for (var i = 3; i < input.length; i++) {
+    if (i > 3 && i < input.length) {
+      name = name + "+" + input[i];
+    }
+    else {
+      name += input[i];
+    }
+  }
+}
 
+function concertThis(name) {
+  if (name === "") {
+    name = "Between The Buried and Me";
+  }
+  // console.log(name)
+  let band = name.split("+").join(" ");
+  // console.log(band)
+  let search = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=codingbootcamp"
+
+  axios.get(search).then(function (response) {
+    console.log("")
+    console.log("Artist: " + band);
+    console.log("---------------------")
+    for (var i = 0; i < 9; i++) {
+      var date = moment(response.data[i].datetime, "YYYY-MM-DDTHH:mm:SS").format("MM/DD/YYYY")
+      console.log(response.data[i].venue.city);
+      console.log(response.data[i].venue.name)
+      console.log(date)
+      console.log("---------------------")
+    }
+  })
+    .catch(function (err) {
+      if (err.response) {
+        console.log("")
+        console.log("Could not find shows for " + band)
+        console.log("")
+      }
+
+    });
+
+}
+
+function spotifyThis(name) {
+  if (name === "") {
+    name = "Ace of Base The Sign";
+  }
+  var song = name.split("+").join("");
+
+  spotify.search({
+    type: "track",
+    query: name,
+    limit: 1
+  }, function (err, data) {
+    if (err) {
+      return console.log("Could not find song: " + err);
+    }
+    console.log("");
+    console.log("-----------------------------------");
+    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+    console.log("Song: " + data.tracks.items[0].name);
+    console.log("Album: " + data.tracks.items[0].album.name);
+    if (data.tracks.items[0].preview_url) {
+      console.log("Sample: " + data.tracks.items[0].preview_url);
+    } else {
+      console.log("Sample:  Sorry no sample available for " + song);
+    }
+    console.log("-----------------------------------");
+    console.log("");
+  });
 
 }
